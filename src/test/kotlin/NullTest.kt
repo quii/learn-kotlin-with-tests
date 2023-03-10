01479a6418a3853904faf55c2955ff4cb39f4776
Chris James <chris.james@springernature.com>
2023-03-10 10:32:14 +0000

    takeif

--- src/test/kotlin/NullTest.kt
+++ src/test/kotlin/NullTest.kt
@@ -1,6 +1,8 @@
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 
 class NullTest {
     @Test
@@ -35,4 +37,27 @@ class NullTest {
         assertThat(foo("abc"), equalTo(3))
         assertThat(foo(null), equalTo(0))
     }
+
+    @Test
+    fun `combining with let is quite common`() {
+        val score: Int? = null
+        val bonus = 2
+        val finalScore = score?.let { it*bonus } ?: 0
+
+        expectThat(finalScore).isEqualTo(0)
+    }
+
+    abstract class BonusMultiplierContract(private val f: (Int) ->Int) {
+        @Test
+        fun `applies bonus multiplier if score is gt 10`() {
+            expectThat(f(10)).isEqualTo(10)
+            expectThat(f(11)).isEqualTo(22)
+        }
+    }
+
+    class TakeIfBonusMultiplier : BonusMultiplierContract({ score -> score.takeIf { it > 10 }?.let { it * 2 } ?: score})
+    class WhenBonusMultiplier : BonusMultiplierContract({score -> when {
+        score > 10 -> score * 2
+        else -> score
+    } })
 }
