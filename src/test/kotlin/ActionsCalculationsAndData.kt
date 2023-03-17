df6616bfa8520b004a267ff7e914e35933462170
Chris James <chris.james@springernature.com>
2023-03-17 15:48:05 +0000

    more notes on actions

--- src/test/kotlin/ActionsCalculationsAndData.kt
+++ src/test/kotlin/ActionsCalculationsAndData.kt
@@ -6,7 +6,7 @@ annotation class Data
 Pure functions, return same data for some input.
 Number of times matters (so println is not a calculation, because calling it twice does have a different effect)
 */
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.PROPERTY, AnnotationTarget.CLASS)
 annotation class Calculation
 
 // All code that isn't a calculation, side effects
@@ -22,7 +22,8 @@ Ideally, we want very few actions.
 If you see something that's an action, you may be able to pull up the action part of it out,
 to further separate out from your domain and convert into a calculation.
 
-Which is easier to re-use, test, etc.
+Which is easier to re-use, test, etc. Calculations are a lot simpler to refactor, moving them around
+can generally be fine, but changing order of actions and stuff is dicier.
  */
 
 @Action
